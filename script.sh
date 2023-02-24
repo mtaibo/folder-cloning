32c5367dae84ae07dac10fcd6bc8a89edf58c5c6
mtaibo <mitaiboperez@gmail.com>
2023-02-24 10:13:56 +0100

    New files

--- script.sh
+++ script.sh
@@ -1,16 +1,8 @@
-
-# Este es el programa que debes ejecutar para hacer el bucle de carpetas 
-#
-# Lo primero es tener un archivo .sh que se llame script.sh y tener también
-# una imagen para meter dentro de las carpetas que se llame saul-goodman.jpg
-#
-# Ya con eso solo tienes que meter el codigo que esta sin los hashtags en el 
-# archivo .sh y ejecutar este comando de aqui abajo en la terminal de debian
-#
-# nohup ./script.sh > output.log 2>&1 &
-
-for i in $(seq 1 10) 
-do 
-  mkdir sexo\ duro\ entre\ enanos\ $i
-  cp saul-goodman.jpg sexo\ duro\ entre\ enanos\ $i
+for n in $(seq 1 5)
+do
+  sleep 10
+  for i in $(seq 1 300)
+  do
+    cp -r  ./sexo\ duro\ de\ enanos ../Desktop/sexo\ duro\ de\ enanos\ $i
+  done
 done
